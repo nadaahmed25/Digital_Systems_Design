3ca2ddf21f38b41350c04db0ec54d0de7c1c69ce
Nada Ahmed <na876983@gmail.com>
2026-08-15 12:17:03 +0300

    Refactor D-flipflop module for clarity

--- Verilog/Counters/Johnson Counter/DFF.v
+++ Verilog/Counters/Johnson Counter/DFF.v
@@ -1,13 +1,10 @@
-// asynchronous reset D-flipflop  
 module d_ff ( d, rstn, clk, q) ; 
 input d, rstn, clk; 
 output reg q; 
-// D-flipflop is sensitive to positive edge clock and negative edge reset 
-always @ (posedge clk or negedge rstn) 
-// the output q is zero at negative reset 
+
+always @ (posedge clk or negedge rstn)
 if (!rstn) 
 q <= 0; 
-// else the output q follows the d input 
 else 
 q <= d; 
 endmodule  
